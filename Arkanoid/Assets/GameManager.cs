cff79ce7822fa7374c97a373c498eeefebe061d7
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-02-20 22:05:10 -0300

    menu para level1

--- Arkanoid/Assets/GameManager.cs
+++ Arkanoid/Assets/GameManager.cs
@@ -1,30 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     // Gerência da pontuação e fluxo do jogo
+    
     void OnGUI () {
-    //GUI.skin = layout;
-    // GUI.Label(new Rect(Screen.width / 2 - 150 - 12, 20, 100, 100), "" + PlayerScore1);
-    // GUI.Label(new Rect(Screen.width / 2 + 150 + 12, 20, 100, 100), "" + PlayerScore2);
+    Scene scene = SceneManager.GetActiveScene();
 
     if (GUI.Button(new Rect(Screen.width / 2 - 60, 350, 120, 53), "PLAY"))
     {
-        // PlayerScore1 = 0;
-        // PlayerScore2 = 0;
-        // theBall.SendMessage("RestartGame", null, SendMessageOptions.RequireReceiver);
+        SceneManager.LoadScene("level1");
     }
-    // if (PlayerScore1 == 10)
-    // {
-    //     GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER ONE WINS");
-    //     theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
-    // } else if (PlayerScore2 == 10)
-    // {
-    //     GUI.Label(new Rect(Screen.width / 2 - 150, 200, 2000, 1000), "PLAYER TWO WINS");
-    //     theBall.SendMessage("ResetBall", null, SendMessageOptions.RequireReceiver);
-    // }
     }
 
     
