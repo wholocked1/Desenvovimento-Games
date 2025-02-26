22d8dd6f6ab59b03565d7f62fe4aeedbc79b06cc
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-02-26 11:44:53 -0300

    comando da plataforma

--- Arkanoid/Assets/Player.cs
+++ Arkanoid/Assets/Player.cs
@@ -7,19 +7,40 @@ public class Player : MonoBehaviour
     public KeyCode moveDir = KeyCode.D;      // Move a raquete para direita
     public KeyCode moveEsq = KeyCode.A;    // Move a raquete para esquerda
     public float speed = 10.0f;             // Define a velocidade da raquete
-public float boundY = 2.25f;            // Define os limites em Y
-private Rigidbody2D rb2d;               // Define o corpo rigido 2D que representa a raquete
+    public float boundX = 10f;            // Define os limites em Y
+    private Rigidbody2D rb2d;               // Define o corpo rigido 2D que representa a raquete
 
 
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();     // Inicializa a raquete
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var vel = rb2d.velocity;                // Acessa a velocidade da raquete
+    if (Input.GetKey(moveDir)) {             // Velocidade da Raquete para ir para cima
+        vel.x = speed;
     }
+    else if (Input.GetKey(moveEsq)) {      // Velocidade da Raquete para ir para cima
+        vel.x = -speed;                    
+    }
+    else {
+        vel.x = 0;                          // Velociade para manter a raquete parada
+    }
+    rb2d.velocity = vel;                    // Atualizada a velocidade da raquete
+
+    var pos = transform.position;           // Acessa a Posição da raquete
+    if (pos.x > boundX) {                  
+        pos.x = boundX;                     // Corrige a posicao da raquete caso ele ultrapasse o limite superior
+    }
+    else if (pos.x < -boundX) {
+        pos.x = -boundX;                    // Corrige a posicao da raquete caso ele ultrapasse o limite inferior
+    }
+    transform.position = pos;               // Atualiza a posição da raquete
+
+ }
 }
