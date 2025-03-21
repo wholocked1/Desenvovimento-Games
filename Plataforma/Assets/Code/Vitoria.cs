5d16f716e591f58119d4c91f56dbc0a4bafc7a5a
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-03-21 16:01:16 -0300

    cena de vitoria

---
+++ Plataforma/Assets/Code/Vitoria.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Vitoria : MonoBehaviour
+{
+    void OnGUI(){
+        Scene scene = SceneManager.GetActiveScene();
+        if(GUI.Button(new Rect(Screen.width / 2 - 60, 350, 120, 53), "INICIO")){
+            SceneManager.LoadScene("Inicio");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
