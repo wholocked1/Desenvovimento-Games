1bfcc1a0dde1e71a5e9e4a9aa54966d14cae92c1
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-03-21 14:00:59 -0300

    Botão Play

---
+++ Plataforma/Assets/Code/Inicial.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inicial : MonoBehaviour
+{
+    void OnGUI(){
+        Scene scene = SceneManager.GetActiveScene();
+
+        if (GUI.Button(new Rect(Screen.width /2 - 60, 350, 120, 53), "PLAY")){
+            SceneManager.LoadScene("Level1");
+        }
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
