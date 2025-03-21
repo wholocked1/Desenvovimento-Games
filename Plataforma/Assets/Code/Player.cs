1b152e0108c245e97f15af85272832aeee6ae11e
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-03-21 15:35:35 -0300

    Tela de Derrota

--- Plataforma/Assets/Code/Player.cs
+++ Plataforma/Assets/Code/Player.cs
@@ -4,24 +4,45 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Sprite[] animationSprites;
+    public Sprite[] animationSprites1;
+    public Sprite[] animationSprites2;
     public float animationTime;
     private SpriteRenderer _spriteRenderer;
     private int _animationFrame;
+    public KeyCode dir = KeyCode.D;
+    public KeyCode esq = KeyCode.A;
+    public float speed = 10.0f;
+    private Rigidbody2D rb2d;
     private void Awake(){
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
     //para começar a animar
     private void Start(){
+        rb2d = GetComponent<Rigidbody2D>();     // Inicializa a raquete
         InvokeRepeating(nameof(AnimateSprite), this.animationTime, this.animationTime);
     }
     //para animar
     private void AnimateSprite(){
-        _animationFrame++;
-        if(_animationFrame >= this.animationSprites.Length){
+        var vel = rb2d.velocity; 
+        if(Input.GetKey(dir)){
+            _animationFrame++;
+        if(_animationFrame >= this.animationSprites2.Length){
             _animationFrame = 0;
         }
-        _spriteRenderer.sprite = this.animationSprites[_animationFrame];
+        _spriteRenderer.sprite = this.animationSprites2[_animationFrame];
+        vel.x = speed;
+        } else if(Input.GetKey(esq)){
+            _animationFrame++;
+        if(_animationFrame >= this.animationSprites1.Length){
+            _animationFrame = 0;
+        }
+        _spriteRenderer.sprite = this.animationSprites1[_animationFrame];
+        vel.x =-speed;
+        }else{
+            vel.x = 0;
+        }
+        rb2d.velocity = vel;
+
     }
     //armamento
     private void OnTriggerEnter2D(Collider2D collision){
