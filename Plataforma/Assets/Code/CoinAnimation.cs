005c6911af7e23dc89ef070014f35f26ae003c91
Ana Carolina Cogo Nami <127544304+wholocked1@users.noreply.github.com>
2025-04-03 20:08:37 -0300

    coin

---
+++ Plataforma/Assets/Code/CoinAnimation.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAnimation : MonoBehaviour
+{
+    public Sprite[] animationSprites;
+    public float animationTime;
+    private int _animationFrame;
+    private SpriteRenderer _spriteRenderer;
+    // Start is called before the first frame update
+    private void Awake(){
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    void Start()
+    {
+        InvokeRepeating(nameof(AnimateSprite), this.animationTime, this.animationTime);
+    }
+
+
+    private void AnimateSprite(){
+        _animationFrame++;
+        if(_animationFrame >= this.animationSprites.Length){
+            _animationFrame = 0;
+        }
+        _spriteRenderer.sprite = this.animationSprites[_animationFrame];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.collider.CompareTag("Player")){
+            Destroy(gameObject);
+        }
+    }
+}
